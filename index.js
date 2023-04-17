4a7f1cbcf555fda8c3dd8286ddb611d523060e85
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-04-17 18:41:33 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api/whoami", (req, res) => {
+let ip = req.ip
+let lang = req.headers["accept-language"]
+let soft = req.headers["user-agent"]
+
+  res.json({ipaddress: ip, language: lang, software: soft});
+
+})
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
